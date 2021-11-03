f7ee5b164bd1a5f541d82fc453821b8027d648a9
Anthony Citraro <acitrar2@kent.edu>
2021-11-03 17:44:37 -0400

    Create 1160_Find_Words_That_Can_Be_formed_by_Characters.java

---
+++ LeetCode/1160_Find_Words_That_Can_Be_formed_by_Characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] charsArray = new int[26];
+        int count = 0;
+        for(char c : chars.toCharArray()){
+            charsArray[c-'a']++;
+        }
+        for(String word : words){
+            int[] wordArray = new int[26];
+            for(char c : word.toCharArray()){
+                wordArray[c-'a']++;
+            }
+            boolean isValid = true;
+            for(int i = 0; i < 26; i++){
+                if(wordArray[i] > 0 && wordArray[i] > charsArray[i]){
+                    isValid = false;
+                }
+            }
+            if(isValid){
+                count += word.length();        
+            }
+        }
+        return count;
+    }
+}
